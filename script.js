c90f543473bba1cc5060d109f18f95fd7ae29cc7
Gauri Raut <gauriraut095@gmail.com>
2026-03-07 19:09:14 +0530

    changes done

---
+++ script.js
@@ -0,0 +1,20 @@
+//Top scroll button
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+};
+
+scrollBtn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+};
+
+
+
